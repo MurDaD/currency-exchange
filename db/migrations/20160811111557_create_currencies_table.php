76cbce86ddbefcf70c46c7e4327d8366a15915a0
Maksym Shakhbazov <maksym@ctp.us>
2016-08-15 12:41:51 +0300

    Update: Added indexes

--- db/migrations/20160811111557_create_currencies_table.php
+++ db/migrations/20160811111557_create_currencies_table.php
@@ -7,11 +7,11 @@ class CreateCurrenciesTable extends AbstractMigration
     public function up()
     {
         $table = $this->table('currencies');
-        $table->addColumn('bank', 'string', array('limit' => 32))
-            ->addColumn('currency', 'string', array('limit' => 40))
+        $table->addColumn('bank', 'string', array('limit' => 32))->addIndex(array('bank'))
+            ->addColumn('currency', 'string', array('limit' => 40))->addIndex(array('currency'))
             ->addColumn('sell', 'float', array('limit' => 6))
             ->addColumn('buy', 'float', array('limit' => 6))
-            ->addColumn('added', 'datetime')
+            ->addColumn('added', 'datetime')->addIndex(array('added'))
             ->save();
     }
 
